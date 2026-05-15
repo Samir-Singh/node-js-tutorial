dfef24bfcd32dcc860e00b6f9c7e0856026ba044
Samir Singh <samirsingh2000.ss@gmail.com>
2026-05-15 17:03:37 +0530

    done

---
+++ Todo-App/backend/index.js
@@ -0,0 +1,49 @@
+import cors from "cors";
+import express from "express";
+import fs from "fs";
+
+const app = express();
+
+let todos = JSON.parse(fs.readFileSync("./todo.json", "utf-8"));
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  }),
+);
+
+// our backend must have this below line otherwise our req.body will be undefined
+// Without this middleware, Express cannot read JSON sent from Axios.
+app.use(express.json());
+
+app.get("/todo", (req, res) => {
+  return res.json(todos);
+});
+
+app.post("/todo", (req, res) => {
+  const body = req.body;
+  const id = Date.now();
+
+  todos.push({ ...body, id });
+  fs.writeFile("./todo.json", JSON.stringify(todos), (err, data) => {
+    if (err) {
+      return res.status(500).json({ status: "error" });
+    }
+
+    return res.json({ status: "success", id });
+  });
+});
+
+app.delete("/todo/:id", (req, res) => {
+  const id = req.params.id;
+  todos = todos?.filter((item) => item?.id != id);
+  fs.writeFile("./todo.json", JSON.stringify(todos), (err, data) => {
+    if (err) {
+      return res.status(500).json({ status: "error" });
+    }
+
+    return res.json({ status: "success", id: Number(id) });
+  });
+});
+
+app.listen(8000, () => console.log("Server Started"));
